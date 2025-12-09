fc2b0acc2c4e36a5808fec067f65f51f9382afe0
mazia777 <youri.poulin.yves@outlook.fr>
2025-12-09 11:59:11 -0400

    mise à jour de boutique.blade.php

--- resources/views/layouts/boutique.blade.php
+++ resources/views/layouts/boutique.blade.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-       <h1 class="text-3xl font-bold underline">
-    <x-navigation/>
-  </h1>
-  <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-  <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
-    </body>
-    </html>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        <main id="content">
+            <x-navigation />
+            <div>
+                @yield('content')
+            </div>
+        </main>
+
+    </h1>
+    <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+</body>
+
+</html>
